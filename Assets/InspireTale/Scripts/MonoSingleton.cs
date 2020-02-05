cb8768a485eebf581299c4d01f8c3cf1ff233a3d
Sahapat <sahapat.starcaft@gmail.com>
2020-02-05 16:00:17 +0700

    [Init] Create InspireTaleFramwork directory. [Modify] -Implement Singleton and MonoSingleton with thread-safety.

---
+++ Assets/InspireTale/Scripts/MonoSingleton.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace InspireTaleFramework
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static bool m_ShuttingDown = false;
+        private static object m_Padlock = new object();
+        private static T m_Instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (m_ShuttingDown)
+                {
+                    Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+                        "' already destroyed. Returning null.");
+                    return null;
+                }
+
+                lock (m_Padlock)
+                {
+                    if (m_Instance == null)
+                    {
+                        m_Instance = (T)FindObjectOfType(typeof(T));
+
+                        if (m_Instance == null)
+                        {
+                            var singletonObject = new GameObject();
+                            m_Instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+                            DontDestroyOnLoad(singletonObject);
+                        }
+                    }
+
+                    return m_Instance;
+                }
+            }
+        }
+        
+        private void OnApplicationQuit()
+        {
+            m_ShuttingDown = true;
+        }
+
+
+        private void OnDestroy()
+        {
+            m_ShuttingDown = true;
+        }
+    }
+}
---
+++ Assets/InspireTale/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿public class Singleton<T> where T:new()
+{
+    private static T m_Instance;
+    private static readonly object m_Padlock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock(m_Padlock)
+            {
+                if(m_Instance==null)
+                {
+                    m_Instance = new T();
+                }
+                return m_Instance;
+            }
+        }
+    }
+}

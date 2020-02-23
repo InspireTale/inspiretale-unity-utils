05221c7b76c65306214cf4b827e36472abba195a
Sahapat <sahapat.starcaft@gmail.com>
2020-02-23 16:59:56 +0700

    [Add] Create CompareUtil

---
+++ Assets/InspireTaleFramework/Scripts/Util/CompareUtil.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompareUtil
+{
+    public static bool CompareFloat(float value1,float value2)
+    {
+        return float.Epsilon > Mathf.Abs(value1) - Mathf.Abs(value2);
+    }
+}

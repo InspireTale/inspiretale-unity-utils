b4b71a68b25983a314abac156697645452c1a546
Sahapat <sahapat.starcaft@gmail.com>
2020-08-29 19:24:57 +0700

    Change function name

--- Assets/InspireTaleFramework/Scripts/Util/RandomUtil.cs
+++ Assets/InspireTaleFramework/Scripts/Util/RandomUtil.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace InspireTaleFramework
 {
     public class RandomUtil
     {
-        public static bool RandomPercenntage(float percentage)
+        public static bool RandomByPercenntage(float percentage)
         {
             return Random.value <= (percentage*0.01);
         }
